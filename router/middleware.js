01a73919ea8be3b396cfb9f97e532890f71c601a
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-15 20:37:33 -0500

    complete with notes in middleware

--- router/middleware.js
+++ router/middleware.js
@@ -20,3 +20,18 @@ function restrict(){
 module.exports = {
     restrict
 }
+
+// must put "username" = previously entered username
+// and "password" = previously entered password
+// in header when making call in insomnia on any route
+// where this restrict function is being used wihtout sessions.
+// in this file const {username, password} = req.headers
+// in video he called middleware.restrict() in router 
+//instead of just restrict()
+
+//with session, we use req.session.user = user in router to handle
+// data needing to access information
+
+// if (!req.session || req.session.user){
+//    return res.status(401).json(authError)
+//}
